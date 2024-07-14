51bccd213b01aff22991a0757002e6ec3edf30f6
Mikhaylov Nikita <nixncom@gmail.com>
2024-07-14 11:35:14 +0300

    Upd

--- upd_repo.sh
+++ upd_repo.sh
@@ -1,3 +1,4 @@
 git pull
+rm $(docker inspect --format="{{.LogPath}}" egts_emu)
 docker restart egts_emu
 curl -X POST -F 'chat_id=288772431' -F 'text=container EGTS EMULATOR restarted' https://api.telegram.org/bot7194357846:AAGfBntMhRcfEpoHPJ0JiVMdXN12FYQUQ4g/sendMessage
